676d16a47da2aad91d2261c58c3937649b7dd92b
michaelmio333 <israeloahimire@gmail.com>
2024-07-27 15:13:00 +0100

    minor fixes

--- Frontend/src/api/MyUserApi.jsx
+++ Frontend/src/api/MyUserApi.jsx
@@ -1,7 +1,7 @@
 import { useMutation, useQuery } from "react-query"
 import { useToast } from "../contexts/ToastContext"
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
 
 export const useValidateMyUser = () => {
     const { addToast } = useToast()
--- Frontend/src/api/SearchApi.jsx
+++ Frontend/src/api/SearchApi.jsx
@@ -1,6 +1,6 @@
 import { useMutation } from "react-query"
 import { useToast } from "../contexts/ToastContext"
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
 
 export const useSearchUsernamesAndRooms = () => {
     const { addToast } = useToast()
